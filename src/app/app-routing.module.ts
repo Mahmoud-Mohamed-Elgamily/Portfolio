c57f04dbb0602e53e5506cfb69a7298b604e3cf1
Mahmoud <mahmoedmohamed19@gmail.com>
2020-05-13 21:45:53 +0200

    add scroll to projects and contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 const routes: Routes = [
   { path: '', component: ProfileComponent },
   { path: 'project/:id', component: ProjectDetailsComponent },
 ];
-
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  // ...any other options you'd like to use
+};
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
